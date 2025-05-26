6dab0dde8376d579e2ebd8dd14594b36926c6314
fabian <fabianalvarez212@gmail.com>
2025-05-25 21:33:13 -0300

    Agregué los dos archivos de clientes a la misma tarea

--- Scripts_SQL/04_crear_clientesR.sql
+++ Scripts_SQL/04_crear_clientesR.sql
@@ -1,14 +1,14 @@
 USE [bd_staging_2025_G06]
 GO
 
-/****** Object:  Table [dbo].[stg_clientesR_G06]    Script Date: 25/05/2025 19:38:24 ******/
+/****** Object:  Table [dbo].[stg_clientes_R_G06]    Script Date: 25/05/2025 21:27:38 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[stg_clientesR_G06](
+CREATE TABLE [dbo].[stg_clientes_R_G06](
 	[CUSTOMER_ID] [int] NULL,
 	[FULL_NAME] [nvarchar](255) NULL,
 	[BIRTH_DATE] [nvarchar](255) NULL,
